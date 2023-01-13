96d929c10148aad0dbeec69e3ce66aa4f82254a5
Michał Szajner <michalszajner@wp.pl>
2023-01-13 21:14:27 +0100

    Search Bar styling and state control over text input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,59 @@
-import {View, StyleSheet, Text} from 'react-native';
+import {StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {FontAwesome5, MaterialCommunityIcons} from '@expo/vector-icons';
+import {useState} from "react";
 
 const SearchBar = () => {
+
+    const [term, setTerm] = useState();
+
+    const handleChange = (term) => {
+        setTerm(term)
+    }
+
     return (<View>
-        <Text>SearchBar</Text>
-    </View>);
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.input} value={term} onChangeText={handleChange}/>
+                <FontAwesome5 name="search" style={styles.icon}/>
+            </View>
+            <TouchableOpacity style={styles.button}>
+                <Text>Search</Text>
+                <MaterialCommunityIcons name="cursor-default-click" size={20} color="black"/>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    button: {
+        padding: 5,
+        textAlign: "center",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignSelf: "center",
+        borderColor: "black",
+        borderWidth: 2,
+        borderRadius: 40,
+        width: 80,
+        marginVertical: 10
+    },
+    inputContainer: {
+        borderColor: "black",
+        borderWidth: 2,
+        borderRadius: 40,
+        height: 40,
+        flexDirection: "row",
+        marginHorizontal: 20
+    },
+    input: {
+        paddingLeft: 10,
+        paddingRight: 5,
+        fontSize: 20,
+        flex: 1,
+    },
+    icon: {
+        padding: 5,
+        fontSize: 24
+    }
+});
 
 export default SearchBar;
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,17 +1,23 @@
 import {View, StyleSheet, Text} from 'react-native';
 import SearchBar from "../components/SearchBar";
 import PhotosList from "../components/PhotosList";
+import { FontAwesome } from '@expo/vector-icons';
 
 const MainScreen = () => {
     return (<View style={styles.main}>
+        <Text style={styles.title}>Search for awesome images <FontAwesome name="image" size={24} color="black" />
+        </Text>
         <SearchBar />
         <PhotosList />
     </View>);
 };
 
 const styles = StyleSheet.create({
-    main: {
-
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        textAlign: "center",
+        padding: 10
     }
 });
 
